04971a4280e43f8a3de45a3f9aad409f1e0f4d39
Karina Yazmin Medina Tovar <79298406+Karina-gif@users.noreply.github.com>
2021-04-22 16:21:13 -0500

    Revert "Revert "Revert "Merge branch 'main' of https://github.com/Woozywicho1/cookie_games_app"""
    This reverts commit 87bcf0137dfacbe6a62d51981a57e654ab7bdb64.

--- upgenero.php
+++ upgenero.php
@@ -5,15 +5,3 @@
 
   mysqli_query($con,"INSERT INTO generos (genero) VALUES ('$genero')")
 ?>
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta http-equiv="Location" content="<?= $url ?>">
-    <script type="text/javascript">
-      window.location.replace("<?= "exito.html" ?>")
-    </script>
-    <title></title>
-  </head>
-  <body></body>
-</html>
